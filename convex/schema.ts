214ca78c8425d0f6bf52aa4bfb2e6cd940a00b88
mselbekk11 <mselbekk11@gmail.com>
2026-01-12 11:21:25 -0800

    updates

---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    tokenIdentifier: v.string(),
+    name: v.string(),
+    email: v.string(),
+  }).index("by_token", ["tokenIdentifier"]),
+  // ... your other tables
+});
