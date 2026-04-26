ae89d7b7b463dd437698291bf8976d0284099cca
mselbekk11 <mselbekk11@gmail.com>
2026-04-25 17:25:18 -0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,11 +118,13 @@ export default function RootLayout({
             </main>
           </ConvexClientProvider>
         </ClerkProvider>
+        <script
+          src="http://localhost:3000/widget/embed.js"
+          data-chatbot-id="jh71vy147ek2swsm0vxr7c1p1985jv31"
+          async
+        ></script>
       </body>
       <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
-      <Script id="chatbase-widget" strategy="lazyOnload">
-        {`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="mg1XIgKMST6xwIdxLTaY-";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`}
-      </Script>
     </html>
   );
 }
